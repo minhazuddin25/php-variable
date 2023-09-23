d35ad6aa176fa5865f8d924d523f9e7592e7d3b4
MinhazUddin25 <hafijminhaz@gmail.com>
2023-09-24 01:33:10 +0600

    added

---
+++ hello.php
@@ -0,0 +1,13 @@
+<?php
+$name = "Bangladesh";
+$age = 25;
+$word = "age";
+$question = "How are you?";
+echo "\n";
+echo 'Hello Bangladesh'.$word;
+echo "\n";
+echo "Hello".$name;
+echo "\n";
+echo "Hello".$name;
+
+echo "Hello {$name}, {$question}";
